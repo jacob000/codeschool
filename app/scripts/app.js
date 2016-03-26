b37a789f931146b1bf790cdfde0b4c1a77ab52d7
jacob000 <dj.jacob93@gmail.com>
2016-03-26 22:08:29 +0100

    Dzialajacy router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,19 +2,29 @@
 
 /* App Module */
 
-var codeschoolApp = angular.module('codeschoolApp', [
-  'ngRoute',
-  'phonecatControllers'
-]);
+var codeschoolApp = angular.module('codeschoolApp', ['ngRoute']);
 
-codeschoolApp.config(['$routeProvider',
-  function($routeProvider) {
+codeschoolApp.config(function($routeProvider) {
     $routeProvider.
+      when('/', {
+        templateUrl : 'views/home.html',
+        controller  : 'mainController'
+      }).
       when('/user/:userID', {
         templateUrl: 'views/user_show.html',
         controller: 'UserShowCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
       });
-  }]);
+    //   otherwise({
+    //     redirectTo: '/phones'
+    //   });
+  });
+  
+  
+ codeschoolApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
+  
+  codeschoolApp.controller('UserShowCtrl', function($scope) {
+        $scope.message = 'Look! I am an about page.';
+    });
